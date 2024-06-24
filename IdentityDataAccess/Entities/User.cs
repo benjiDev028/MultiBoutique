51cf265d3c2c9c70ccfeaa8c8d0c4f22c04a1016
benjamin manda ilunga <benjaminilunga011@gmail.com>
2024-06-24 09:46:29 -0400

    IUserRepository finish

---
+++ IdentityDataAccess/Entities/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityDataAccess.Entities
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public Role UserRole { get; set; }
+        public Guid? BoutiqueId { get; set; }
+        public string RefreshToken { get; set; } = string.Empty;
+        public byte[]? PasswordHash { get; set; }
+        public byte[]? PasswordSalt { get; set; }
+        public int ConfirmationCode { get; set; }
+    }
+
+    public enum Role
+    {
+        Client,
+        Gerante,
+        SuperAdmin
+    }
+
+
+}
+}
---
+++ IdentityDataAccess/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using IdentityDataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityDataAccess.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User> GetByIdAsync(Guid id);
+        Task <List<User>> GetAllAsync();
+        Task DeleteAsync(Guid id);
+
+
+    }
+}
